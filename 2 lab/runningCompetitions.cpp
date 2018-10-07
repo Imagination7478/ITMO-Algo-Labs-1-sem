98ad150aed39ac6698b3203235ab176699e929da
Dmitry <dimka40302010@gmail.com>
2018-10-07 21:31:00 +0300

    replace

--- 2 lab/runningCompetitions.cpp
+++ 2 lab/runningCompetitions.cpp
@@ -1,34 +1,29 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-void merge(vector<class Runners> &arrRunners, int left, int middle, int right)
+void merge(vector<string> &countries, vector<string> &surnames, int left, int middle, int right)
 {
 	int i, j, k;
 	int n1 = middle - left + 1;
 	int n2 = right - middle;
 
-	vector<class Runners> L;
-	vector<class Runners> R;
-	Runners runner;
-
+	vector<string> Lcountries;
+	vector<string> Lsurnames;
+	vector<string> Rcountries;
+	vector<string> Rsurnames;
 	for (i = 0; i < n1; i++)
 	{
-		runner.setCountry(arrRunners[left + i].getCountry());
-		runner.setSurname(arrRunners[left + i].getSurname());
-		runner.setPlace(arrRunners[left + i].getPlace());
-
-		L.push_back(runner);
+		//Заполняем L
+		Lcountries.push_back( countries[left + i] );
+		Lsurnames.push_back( surnames[left + i] );
 	}
-	for (i = 0; i < n2; i++)
+	for (j = 0; j < n2; j++)
 	{
-		runner.setCountry(arrRunners[middle + 1 + i].getCountry());
-		runner.setSurname(arrRunners[middle + 1 + i].getSurname());
-		runner.setPlace(arrRunners[middle + 1 + i].getPlace());
-
-		R.push_back(runner);
+		//Заполняем R
+		Rcountries.push_back( countries[middle + 1 + j] );
+		Rsurnames.push_back( surnames[middle + 1 + j] );
 	}
 
 	i = 0;
@@ -36,157 +31,94 @@ void merge(vector<class Runners> &arrRunners, int left, int middle, int right)
 	k = left;
 	while (i < n1 && j < n2)
 	{
-		if (L[i].getCountry() <= R[j].getCountry())
+		if (Lcountries[i] <= Rcountries[j])
 		{
-			arrRunners[k].setCountry(L[i].getCountry());
+			countries[k] = Lcountries[i];
+			surnames[k] = Lsurnames[i];
+			k++;
 			i++;
 		}
 		else
 		{
-			arrRunners[k].setCountry(R[j].getCountry());
+			countries[k] = Rcountries[j];
+			surnames[k] = Rsurnames[j];
+			k++;
 			j++;
 		}
-		k++;
 	}
 
 	while (i < n1)
 	{
-		arrRunners[k].setCountry(L[i].getCountry());
-		i++;
+		countries[k] = Lcountries[i];
+		surnames[k] = Lsurnames[i];
 		k++;
+		i++;
 	}
-
 	while (j < n2)
 	{
-		arrRunners[k].setCountry(R[j].getCountry());
-		j++;
+		countries[k] = Rcountries[j];
+		surnames[k] = Rsurnames[j];
 		k++;
+		j++;
 	}
 }
 
-void mergeSort(vector<class Runners> &arrRunners, int left, int right)
+void mergeSort(vector<string> &countries, vector<string> &surnames, int left, int right)
 {
-	if (left < right)
+	if (right > left)
 	{
-		int middle = left + (right - left) / 2;
+		int middle = (right + left) / 2;
+		mergeSort(countries, surnames, left, middle);
+		mergeSort(countries, surnames, middle + 1, right);
 
-		mergeSort(arrRunners, left, middle);
-		mergeSort(arrRunners, middle + 1, right);
-
-		merge(arrRunners, left, middle, right);
+		merge(countries, surnames, left, middle, right);
 	}
 }
-
-class Runners
-{
-private:
-	string surname;
-	int place;
-	string country;
-
-public:
-	//friend void merge(vector<Runners> &arrRunners, int left, int middle, int right);
-	//friend void mergeSort(vector<Runners> &arrRunners, int left, int right);
-
-	Runners operator = (Runners &runner)
-	{
-		this->country = runner.country;
-		this->surname = runner.surname;
-		this->place = runner.place;
-		return runner;
-	}
-
-	void setSurname(string surname)
-	{
-		this->surname = surname;
-	}
-	void setPlace(int place)
-	{
-		this->place = place;
-	}
-	void setCountry(string country)
-	{
-		this->country = country;
-	}
-	string getSurname()
-	{
-		return this->surname;
-	}
-	string getCountry()
-	{
-		return this->country;
-	}
-	int getPlace()
-	{
-		return this->place;
-	}
-};
-
-
 int main()
 {
-	int quantityRunners = 0;
-	vector<Runners> arrRunners;
-	Runners runner;
-
+	vector<string> arrOfSurnamesRunners;
+	vector<string> arrOfCountriesRunners;
 	fstream fs;
+
 	fs.open("race.in", fstream::in);
-	fs >> quantityRunners;
-
-	string surname, country;
-	for (int i = 0; i < quantityRunners; i++)
+	string surname;
+	string country;
+	int size;
+	fs >> size;
+	for (int i = 0; i < size; ++i)
 	{
-		runner.setPlace(i + 1);
-
 		fs >> country;
-		runner.setCountry(country);
+		arrOfCountriesRunners.push_back(country);
 
 		fs >> surname;
-		runner.setSurname(surname);
-
-		arrRunners.push_back(runner);
+		arrOfSurnamesRunners.push_back(surname);
 	}
 
 	fs.close();
 
-	// Сорт. вставками
-	/*for (int i = 1; i < quantityRunners; i++)
-	{	
-		for (int j = i; j > 0; j--)
-		{
-			if (arrRunners[j - 1].getCountry() > arrRunners[j].getCountry())
-			{
-				swap(arrRunners[j - 1], arrRunners[j]);
-			}
-		}
-	}*/
-
-	mergeSort(arrRunners, 0, arrRunners.size() - 1);
-
-	
-
-	//sort(arrRunners.begin(), arrRunners.end());
+	mergeSort(arrOfCountriesRunners, arrOfSurnamesRunners, 0, arrOfCountriesRunners.size() - 1);
 
 	fs.open("race.out", fstream::out);
-
-	for (int i = 0; i < quantityRunners; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (i == 0)
 		{
-			fs << "=== " << arrRunners[i].getCountry() << " ===" << endl;
-			fs << arrRunners[i].getSurname() << endl;
-		} 
-		else if (arrRunners[i].getCountry() == arrRunners[i - 1].getCountry())
+			fs << "=== " << arrOfCountriesRunners[i] << " ===" << endl;
+			fs << arrOfSurnamesRunners[i] << endl;
+		}
+		else if (arrOfCountriesRunners[i] == arrOfCountriesRunners[i - 1])
 		{
-			fs << arrRunners[i].getSurname() << endl;
+			fs << arrOfSurnamesRunners[i] << endl;
 		}
 		else
 		{
-			fs << "=== " << arrRunners[i].getCountry() << " ===" << endl;
-			fs << arrRunners[i].getSurname() << endl;
+			fs << "=== " << arrOfCountriesRunners[i] << " ===" << endl;
+			fs << arrOfSurnamesRunners[i] << endl;
 		}
 	}
-
 	fs.close();
+
+
+
 	return 0;
 }
