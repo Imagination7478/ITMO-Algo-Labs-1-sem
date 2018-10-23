2669cd781131ad246ee1f2dfbc200aa987377aec
Dmitry <dimka40302010@gmail.com>
2018-10-23 22:25:17 +0300

    Update isHeap

--- 3 lab/isHeap.cpp
+++ 3 lab/isHeap.cpp
@@ -1,21 +1,16 @@
 #include <fstream>
 #include <vector>
-
-static int k = 0;
+using namespace std;
 
 bool isParent(vector<int> arr, int parent)
 {
-	indexOfFirstChild = 2 * parent;
-	indexOfSecondChild = 2 * parent + 1;
+	int indexOfFirstChild = 2 * parent + 1;
+	int indexOfSecondChild = 2 * parent + 2;
 	
 	if (indexOfFirstChild >= arr.size())
-	{
-		
-	}
+	{}
 	else if (indexOfSecondChild >= arr.size())
-	{
-		
-	}
+	{}
 	else if (arr[indexOfFirstChild] > arr[parent] && arr[indexOfSecondChild] > arr[parent])
 	{
 		return true;
@@ -33,6 +28,7 @@ int main()
 	
 	int size = 0, temp = 0;
 	vector<int> arr;
+	fs >> size;
 	
 	for(int i = 0; i < size; i++)
 	{
@@ -42,15 +38,19 @@ int main()
 	
 	fs.close();
 	
+	fs.open("isheap.out", fstream::out);
 	for(int i = 0; i < size; i++)
 	{
-		if (isParent(arr, i)
+		if (isParent(arr, i))
+		{}
 		else 
 		{
-			fs.open("isheap.out", fstream::out);
 			fs << "NO";
+			break;
 		}
+		if (i == size - 1) { fs << "YES"; }
 	}
+	fs.close();
 	
 	
 	return 0;
